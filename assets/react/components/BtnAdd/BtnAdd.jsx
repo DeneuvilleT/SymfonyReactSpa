1c613d9fea58113de168200f8b0446a1c0cc9b13
DeneuvilleT <deneuville.thomas@gmail.com>
2023-10-29 17:58:27 +0100

    wip - Gestion des quantité max du quickAdd

--- assets/react/components/BtnAdd/BtnAdd.jsx
+++ assets/react/components/BtnAdd/BtnAdd.jsx
@@ -10,7 +10,9 @@ const BtnAdd = ({ product }) => {
 
   const dispatch = useDispatch();
 
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(0);
+  const [stockAvailable, setStockAvailable] = useState(product.stock);
+  const [maxStockAvailable, setMaxStockAvailable] = useState(null);
   const [stock, setStock] = useState(true);
 
   useEffect(() => {
@@ -19,33 +21,66 @@ const BtnAdd = ({ product }) => {
 
   const handleAddToCart = (e) => {
     e.preventDefault();
-
-    dispatch(addToCart({ product: product, quantity: quantity }));
-    return checkStock();
+    if (quantity > 0) {
+      dispatch(addToCart({ product: product, quantity: quantity }));
+      setQuantity(0);
+      /**
+       * ATTENTION - Quand il ne reste que 1 de possible en quantité posssiblke alors 0 en stockInitial
+       * Gérer le panier par cookie
+       */
+      // setMaxStockAvailable(stockAvailable);
+      return checkStock();
+    }
   };
 
-  /**
-   * Faire que la quantité possible du quickAdd s'adapte à la quantité du produit déjà dans le panier
-   */
+  const checkAvalaibleStock = () => {
+    // L'initial stock doit être la différence qu'il y aentre le stock disponible du produit et la quantité déjà présente dans le panier
+    const index = cart.findIndex((item) => item.id === product.id);
+    if (index >= 0 && product.stock !== 0) {
+      const newAvailableStock = product.stock - cart[index].item_quantity;
+      setMaxStockAvailable(newAvailableStock);
+      return setStockAvailable(newAvailableStock);
+    }
+  };
 
   const checkStock = () => {
     const index = cart.findIndex((item) => item.id === product.id);
 
     if (index >= 0 && quantity + cart[index].item_quantity >= product.stock) {
-      return setStock(false);
+      setStock(false);
     } else if (quantity >= product.stock) {
-      return setStock(false);
+      setStock(false);
     }
+    return checkAvalaibleStock();
   };
 
   const handleAddQuantity = (e) => {
     e.preventDefault();
-    return setQuantity(quantity < product.stock ? quantity + 1 : quantity);
+
+    if (quantity < product.stock) {
+      if (maxStockAvailable === null) {
+        setQuantity(quantity < product.stock ? quantity + 1 : quantity);
+        return setStockAvailable(
+          quantity < product.stock ? stockAvailable - 1 : stockAvailable
+        );
+      } else {
+        if (
+          (quantity <= stockAvailable && quantity < maxStockAvailable) ||
+          (quantity >= stockAvailable && quantity < maxStockAvailable)
+        ) {
+          setQuantity(quantity + 1);
+          return setStockAvailable(stockAvailable - 1);
+        }
+      }
+    }
   };
 
   const handleLessQuantity = (e) => {
     e.preventDefault();
-    return setQuantity(quantity > 1 ? quantity - 1 : quantity);
+    setQuantity(quantity >= 1 ? quantity - 1 : quantity);
+    return setStockAvailable(
+      quantity >= 1 ? stockAvailable + 1 : stockAvailable
+    );
   };
 
   return (
@@ -83,6 +118,11 @@ const BtnAdd = ({ product }) => {
                 onClick={(e) => handleLessQuantity(e)}
               />
             </div>
+
+            <p style={{ fontSize: "1.5em" }}>
+              / {stockAvailable}{" "}
+              <span style={{ fontWeight: "400" }}>dispo</span>
+            </p>
           </section>
         </aside>
       ) : (
