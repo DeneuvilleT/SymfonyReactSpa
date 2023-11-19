30a211538410e0a2cb222564acffad58db790462
DeneuvilleT <deneuville.thomas@gmail.com>
2023-11-19 13:59:34 +0100

    wip - Page product

--- assets/react/components/Product/Product.jsx
+++ assets/react/components/Product/Product.jsx
@@ -2,10 +2,7 @@ import React, { useState, useEffect, Fragment } from "react";
 
 import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  getAllProducts,
-  getProductsStatus,
-} from "../../Store/slices/productsSlices";
+import { getAllProducts, getProductsStatus } from "../../Store/slices/productsSlices";
 import { addToCart } from "../../Store/slices/cartSlices";
 
 import styles from "./product.styles.scss";
@@ -19,6 +16,10 @@ const Product = () => {
   const products = useSelector(getAllProducts);
   const productsStatus = useSelector(getProductsStatus);
 
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
+
   useEffect(() => {
     setProduct(products.find((x) => x.id === Number(params.id)));
   }, [productsStatus, dispatch]);
@@ -29,56 +30,34 @@ const Product = () => {
         <article itemScope itemType="http://schema.org/Product">
           <h2 itemProp="name">{product.title}</h2>
 
-          <meta
-            itemProp="image"
-            content={`${location.origin}/uploads/images/${product.cover}`}
-          />
-          <img
-            style={{ width: "200px", height: "auto" }}
-            src={`${location.origin}/uploads/images/${product.cover}`}
-            alt={product.title}
-          />
+          <section>
+            <div>
+              <meta itemProp="image" content={`${location.origin}/uploads/images/${product.cover}`} />
+              <img style={{ width: "200px", height: "auto" }} src={`${location.origin}/uploads/images/${product.cover}`} alt={product.title} />
+            </div>
 
-          <p itemProp="brand">{product.brand}</p>
-          <span itemProp="sku">
-            <strong>ref:</strong> {product.sku}
-          </span>
+            <div>
+              <p itemProp="brand">{product.brand}</p>
+              <span itemProp="sku">
+                <strong>ref:</strong> {product.sku}
+              </span>
 
-          <Fragment>
-            <p
-              itemProp="description"
-              dangerouslySetInnerHTML={{ __html: product.description }}
-            />
-          </Fragment>
-
-          <Link
-            onClick={() => {
-              dispatch(addToCart(product));
-              // Vérifier la quan
-            }}
-            to={"/cart"}
-          >
-            Ajouter au panier
-          </Link>
-
-          {/* <form action={`/api/stripe/product/${product.id}`} method="get">
-            <input type="submit" value="Acheter" />
-          </form> */}
+              <Fragment>
+                <p itemProp="description" dangerouslySetInnerHTML={{ __html: product.description }} />
+              </Fragment>
+            </div>
+          </section>
 
           <div itemProp="offers" itemScope itemType="http://schema.org/Offer">
             <meta itemProp="price" content={product.priceUnit} />
             <meta itemProp="priceCurrency" content="€" />
+            <button onClick={() => handleAddToCart(product)} to={"/cart"}>
+              Ajouter au panier
+            </button>
             <p>
-              <strong>Prix :</strong>{" "}
-              {(Number(product.priceUnit) / 100).toFixed(2)} €
+              <strong>Prix :</strong> {(Number(product.priceUnit) / 100).toFixed(2)} €
             </p>
 
-            {/*
-             * Gérer le cummul d'un stock d'article ajouté au panier vie le quickview
-             *
-             * Toujours vérifier la quantité dans le panier avant l'affichage
-             *
-             */}
             <meta itemProp="availability" content="http://schema.org/InStock" />
             <p>
               <strong>Stock :</strong> {product.stock}
