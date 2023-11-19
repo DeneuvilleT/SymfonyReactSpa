3b518a68240f75858b38d7254947cdf665d65b47
DeneuvilleT <deneuville.thomas@gmail.com>
2023-11-19 19:32:44 +0100

    wip - Quantity page product

--- assets/react/components/Product/Product.jsx
+++ assets/react/components/Product/Product.jsx
@@ -4,6 +4,7 @@ import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllProducts, getProductsStatus } from "../../Store/slices/productsSlices";
 import { addToCart } from "../../Store/slices/cartSlices";
+import { Icon } from "@iconify/react";
 
 import styles from "./product.styles.scss";
 
@@ -28,12 +29,16 @@ const Product = () => {
     <main className={styles.product}>
       {product !== null && typeof product !== "undefined" && (
         <article itemScope itemType="http://schema.org/Product">
-          <h2 itemProp="name">{product.title}</h2>
+          <h2 itemProp="name">
+            <span></span>
+            {product.title}
+            <span></span>
+          </h2>
 
           <section>
             <div>
               <meta itemProp="image" content={`${location.origin}/uploads/images/${product.cover}`} />
-              <img style={{ width: "200px", height: "auto" }} src={`${location.origin}/uploads/images/${product.cover}`} alt={product.title} />
+              <img src={`${location.origin}/uploads/images/${product.cover}`} alt={product.title} />
             </div>
 
             <div>
@@ -45,23 +50,34 @@ const Product = () => {
               <Fragment>
                 <p itemProp="description" dangerouslySetInnerHTML={{ __html: product.description }} />
               </Fragment>
+
+              <p>
+                <Icon icon="vaadin:stock" color="black" width="50" height="50" />
+                <strong>Stock :</strong>
+                {product.stock}
+              </p>
             </div>
           </section>
 
           <div itemProp="offers" itemScope itemType="http://schema.org/Offer">
             <meta itemProp="price" content={product.priceUnit} />
             <meta itemProp="priceCurrency" content="€" />
+            <p>{(Number(product.priceUnit) / 100).toFixed(2)} €</p>
+
+            <aside className={styles.move_quantity}>
+              <Icon icon="line-md:minus-circle" width="35" height="35" />
+              {/*  */}
+              <p>1</p>
+              {/*  */}
+              <Icon icon="line-md:plus-circle" width="35" height="35" />
+            </aside>
+
             <button onClick={() => handleAddToCart(product)} to={"/cart"}>
-              Ajouter au panier
+              Ajouter au panier{" "}
+              <Icon icon={product.stock !== 0 ? "line-md:download-outline-loop" : "line-md:download-off-loop"} color="white" width="45" height="45" />
             </button>
-            <p>
-              <strong>Prix :</strong> {(Number(product.priceUnit) / 100).toFixed(2)} €
-            </p>
 
             <meta itemProp="availability" content="http://schema.org/InStock" />
-            <p>
-              <strong>Stock :</strong> {product.stock}
-            </p>
           </div>
         </article>
       )}
